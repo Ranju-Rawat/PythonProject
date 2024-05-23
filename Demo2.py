b08c661116c9a9c35736a524b651851e09ac6c97
Ranju Rawat <rawatranju3@gmail.com>
2024-05-24 00:33:38 +0530

    new file requirement.txt has been added

--- Demo2.py
+++ Demo2.py
@@ -1,1 +1,16 @@
+import pytest
+
+class Test_methods:
+    @pytest.mark.dependency()
+    def test_demo1(self):
+        a = "Hello world"
+        print("Print demo1")
+
+    def test_demo2(self):
+        print("print demo2")
+
+    @pytest.mark.dependency(depends=["Test_methods::test_demo1"])
+    def test_demo3(self):
+        print("Print demo 3")
+
 
